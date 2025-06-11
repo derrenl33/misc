4aaf81fb6f986742f5ded7d9c16ca051c9f66fd4
derrenl33 <101106740+derrenl33@users.noreply.github.com>
2025-06-11 16:14:23 -0400

    revise prompts

--- max-diff-between-even-odd-freq.java
+++ max-diff-between-even-odd-freq.java
@@ -1,14 +1,10 @@
-/*
-
-You are given a string s consisting of lowercase English letters.
+/*   You are given a string s consisting of lowercase English letters.
 
 Your task is to find the maximum difference diff = freq(a1) - freq(a2) between the frequency of characters a1 and a2 in the string such that:
 
 a1 has an odd frequency in the string.
 a2 has an even frequency in the string.
-Return this maximum difference.
-
-*/
+Return this maximum difference.   */
 
 class Solution {
     public int maxDifference(String s) {
--- min-value-for-pos-step-by-step-sum.java
+++ min-value-for-pos-step-by-step-sum.java
@@ -1,10 +1,8 @@
-package com.mycompany.problemsolving;
-
-/*Given an array of integers nums, you start with an initial positive value startValue.
+/*   Given an array of integers nums, you start with an initial positive value startValue.
 
 In each iteration, you calculate the step by step sum of startValue plus elements in nums (from left to right).
 
-Return the minimum positive value of startValue such that the step by step sum is never less than 1. */
+Return the minimum positive value of startValue such that the step by step sum is never less than 1.   */
 
 public class solution {
     public static int minStartValue(int[] nums) {
